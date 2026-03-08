e917808b973b55d0b98ec047d6e05807275f33f2
Nauto14 <97150125+Nauto14@users.noreply.github.com>
2026-03-08 18:54:54 +0100

    Add ESLint configuration for Next.js

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import { defineConfig } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const eslintConfig = defineConfig([...nextVitals]);
+export default eslintConfig;
